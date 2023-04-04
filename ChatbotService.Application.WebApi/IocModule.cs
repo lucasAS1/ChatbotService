8291286fd5f505e1f0ed09064ec3d3a135cafdf3
lucasAS1 <lucas.a.silva1@outlook.com>
2023-04-03 21:28:34 -0300

    updating chatbot project library

--- ChatbotService.Application.WebApi/IocModule.cs
+++ ChatbotService.Application.WebApi/IocModule.cs
@@ -26,6 +26,7 @@ public class IocContainer : Module
     private static void ConfigureInfrastructureLayer(ContainerBuilder builder)
     {
         builder.RegisterType<BotFrameworkAgent>().As<IBotFrameworkAgent>();
+        builder.RegisterType<Context>().As<IContext>();
     }
     
     private static void ConfigureDomainLayer(ContainerBuilder builder)
@@ -38,22 +39,7 @@ public class IocContainer : Module
 
     private static void ConfigureDbRepositories(ContainerBuilder builder)
     {
-        builder.Register<Repository<Conversation>>(c =>
-            {
-                var config = c.Resolve<IOptions<Settings>>();
-                
-                //dear god why have u forsaken us ...
-                //i need to do this because for some reason when the app is in azure app service it
-                //completely destroys this connection string. I think it might me something with the 'mongodb+srv' stuff
-                config.Value.MongoDbSettings.Url = $"mongodb+srv://{config.Value.MongoDbSettings.Url}";
-                return new Repository<Conversation>(config);
-            })
-            .As<IRepository<Conversation>>();
-        builder.Register<Repository<Message>>(c =>
-            {
-                var config = c.Resolve<IOptions<Settings>>();
-                return new Repository<Message>(config);
-            })
-            .As<IRepository<Message>>();
+        builder.RegisterType<Repository<Conversation>>().As<IRepository<Conversation>>();
+        builder.RegisterType<Repository<Message>>().As<IRepository<Message>>();
     }
 }
